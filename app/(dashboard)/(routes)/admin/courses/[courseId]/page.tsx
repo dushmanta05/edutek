7da60ed82e0bb64e9f3b046d92f6babae45f92fa
Dushmanta <dushmanta.dev@gmail.com>
2024-01-01 14:59:13 +0530

    feat: develop category edit section

--- app/(dashboard)/(routes)/admin/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/admin/courses/[courseId]/page.tsx
@@ -6,6 +6,7 @@ import { LayoutDashboard } from "lucide-react";
 import TitleForm from "./_components/title-form";
 import DescriptionForm from "./_components/description-form";
 import ImageForm from "./_components/image-form";
+import CategoryForm from "./_components/category-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -19,6 +20,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
       id: params.courseId,
     },
   });
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  console.log(categories);
 
   if (!course) {
     return redirect("/");
@@ -46,15 +55,23 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
           </span>
         </div>
       </div>
-      <div className="grif grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
         <div>
           <div className="flex items-center gap-x-2">
             <IconBadge icon={LayoutDashboard} />
-            <h2 className="text-xl">Customise your course</h2>
+            <h2 className="text-xl">Customize your course</h2>
           </div>
           <TitleForm initialData={course} courseId={course.id} />
           <DescriptionForm initialData={course} courseId={course.id} />
           <ImageForm initialData={course} courseId={course.id} />
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.id,
+            }))}
+          />
         </div>
       </div>
     </div>
---
+++ scripts/seed.ts
@@ -0,0 +1,25 @@
+const { PrismaClient } = require("@prisma/client");
+
+const database = new PrismaClient();
+
+async function main() {
+  try {
+    await database.category.createMany({
+      data: [
+        { name: "TypeScript" },
+        { name: "JavaScript" },
+        { name: "Ruby" },
+        { name: "Python" },
+        { name: "Java" },
+        { name: "MySQL" },
+      ],
+    });
+    console.log("Success");
+  } catch (error) {
+    console.log("Error seeding the database categories", error);
+  } finally {
+    await database.$disconnect();
+  }
+}
+
+main();
