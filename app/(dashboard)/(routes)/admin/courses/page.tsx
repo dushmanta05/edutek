f6e9ed6c50afd66e3f2af01faabe3ceb6c71ee4b
Dushmanta <dushmanta0511@gmail.com>
2024-01-02 20:56:07 +0530

    feat: display data table

--- app/(dashboard)/(routes)/admin/courses/page.tsx
+++ app/(dashboard)/(routes)/admin/courses/page.tsx
@@ -1,12 +1,29 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const CoursesPage = () => {
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { db } from "@/lib/db";
+
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/admin/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
