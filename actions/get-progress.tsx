3f49de1b353cdbd0ec5f8ee66dc259835b287b4c
Dushmanta <dushmanta0511@gmail.com>
2024-01-03 04:14:03 +0530

    feat(search): add Course grid

---
+++ actions/get-progress.tsx
@@ -0,0 +1,38 @@
+import { db } from "@/lib/db";
+
+export const getProgress = async (
+  userId: string,
+  courseId: string
+): Promise<number> => {
+  try {
+    const publishedChapters = await db.chapter.findMany({
+      where: {
+        courseId: courseId,
+        isPublished: true,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    const publishedChaptersIds = publishedChapters.map((chapter) => chapter.id);
+
+    const validCompletedChapter = await db.userProgress.count({
+      where: {
+        userId: userId,
+        chapterId: {
+          in: publishedChaptersIds,
+        },
+        isCompleted: true,
+      },
+    });
+
+    const progressPercentage =
+      (validCompletedChapter / publishedChaptersIds.length) * 100;
+
+    return progressPercentage;
+  } catch (error) {
+    console.log("[GET_PROGRESS]", error);
+    return 0;
+  }
+};
