9212338c94813be0c2d08bcf8b4f6d95cd8d0f97
Dushmanta <dushmanta.dev@gmail.com>
2024-01-02 12:17:07 +0530

    feat(chapter): create and publish

---
+++ components/custom/preview.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useMemo } from "react";
+import dynamic from "next/dynamic";
+
+import "react-quill/dist/quill.bubble.css";
+
+interface PreviewProps {
+  value: string;
+}
+
+const Preview = ({ value }: PreviewProps) => {
+  const ReactQuill = useMemo(
+    () => dynamic(() => import("react-quill"), { ssr: false }),
+    []
+  );
+  return (
+    <div className="bg-white">
+      <ReactQuill theme="bubble" value={value} readOnly />
+    </div>
+  );
+};
+
+export default Preview;
