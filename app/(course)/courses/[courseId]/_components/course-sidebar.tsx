963f2295e9a8ba95b5f1643c3a18d5ec4b07abce
Dushmanta <dushmanta0511@gmail.com>
2024-01-03 13:41:37 +0530

    feat(course): add chapter progress functionality

--- app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -1,7 +1,10 @@
-import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { Chapter, Course, UserProgress } from "@prisma/client";
 import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+
+import { CourseProgress } from "@/components/custom/course-progress";
 import CourseSidebarItem from "./course-sidebar-item";
 
 interface CourseSidebarProps {
@@ -33,6 +36,11 @@ const CourseSidebar = async ({ course, progressCount }: CourseSidebarProps) => {
     <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
       <div className="p-8 flex flex-col border-b">
         <h1 className="font-semibold">{course.title}</h1>
+        {purchase && (
+          <div className="mt-10">
+            <CourseProgress variant="success" value={progressCount} />
+          </div>
+        )}
       </div>
       <div className="flex flex-col w-full">
         {course.chapters.map((chapter) => (
