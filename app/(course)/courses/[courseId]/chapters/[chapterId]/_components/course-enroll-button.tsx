156851ea78972d7cbdf5a366b4583880e7ee5bbc
Dushmanta <dushmanta0511@gmail.com>
2024-01-03 13:03:14 +0530

    feat(payment): add stripe integration

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -13,8 +13,29 @@ interface CourseEnrollButtonProps {
 }
 
 const CourseEnrollButton = ({ price, courseId }: CourseEnrollButtonProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
+
+      window.location.assign(response.data.url);
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <Button size="sm" className="w-full md:w-auto">
+    <Button
+      onClick={onClick}
+      disabled={isLoading}
+      size="sm"
+      className="w-full md:w-auto"
+    >
       Enroll for {formatPrice(price)}
     </Button>
   );
