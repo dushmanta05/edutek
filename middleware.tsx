a20b7e321ea475c7049a98c029183067a0ae543b
Dushmanta <dushmanta.dev@gmail.com>
2024-01-03 13:03:14 +0530

    feat(payment): add stripe integration

--- middleware.tsx
+++ middleware.tsx
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/test", "/api/uploadthing"],
+  publicRoutes: ["/test", "/api/uploadthing", "api/webhook"],
 });
 
 export const config = {
